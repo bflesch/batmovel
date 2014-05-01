0621cf890b69cc8632ea5faa4273f26f2cabb013
Lucas <lucas@ilhadesconhecida>
2014-04-30 21:30:20 -0300

    problema de date e timezone corrigido

--- Batmovel/src/br/usp/caronas/RideFormActivity.java
+++ Batmovel/src/br/usp/caronas/RideFormActivity.java
@@ -34,7 +34,7 @@ public class RideFormActivity extends Activity {
 	public int selected_hour;
 	public int selected_minute;
 	
-	SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+	SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
